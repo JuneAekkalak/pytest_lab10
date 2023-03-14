b6967a256e9b041e508e8be7d1482d76acc3a05a
Aekkalak Junjongkon <111352216+JuneAekkalak@users.noreply.github.com>
2023-03-14 14:30:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World1"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
